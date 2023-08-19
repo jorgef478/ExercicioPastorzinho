2f7345c07e8a97abc7a387ef7af85ba32a0d1bd8
= <=>
2023-08-18 21:49:23 -0300

    Exercicio feito 18-08 Ermogenes

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Thread.Sleep(200);
+Console.WriteLine("Esta e a musica do pastorzinho:");
+Thread.Sleep(500);
+
+Console.Write("Do, ");
+Console.Beep(1320, 300);
+Console.Write("Re, ");
+Console.Beep (1485,300); 
+Console.Write("Mi, ");
+Console.Beep(1650, 300) ;
+Console.Write("Fa, ");
+Console.Beep (1759, 600);
+Console.Write("Fa, ");
+Console.Beep(1759,700);
+Console.WriteLine("Fa");
+Console.Beep (1759,700);
+
+Console.Write("Do, ");
+Console.Beep(1320,300);
+Console.Write("Re, ");
+Console.Beep(1485,300);
+Console.Write("Do, ");
+Console.Beep(1320,300);
+Console.Write("Re, ");
+Console.Beep(1485,600);
+Console.Write("Re, ");
+Console.Beep(1485,700);
+Console.WriteLine("Re, ");
+Console.Beep(1485,700);
+
+Console.Write("Do, ");
+Console.Beep(1320,300);
+Console.Write("Sol, ");
+Console.Beep(1980,300);
+Console.Write("Fa, ");
+Console.Beep(1759,300);
+Console.Write("Mi, ");
+Console.Beep(1650,600);
+Console.Write("Mi, ");
+Console.Beep(1650,500);
+Console.WriteLine("Mi");
+Console.Beep(1650,500);
+
+Console.Write("Do, ");
+Console.Beep(1320,300);
+Console.Write("Re, ");
+Console.Beep(1485,300);
+Console.Write("Mi, ");
+Console.Beep(1650,300);
+Console.Write("Fa, ");
+Console.Beep(1759,600);
+Console.Write("Fa, ");
+Console.Beep(1759,500);
+Console.Write("Fa");
+Console.Beep(1759,500);
